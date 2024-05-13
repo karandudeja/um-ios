49a9877cbeb6bf1e7b3270db1362c10e253eea8a
karandudeja <learning2make@gmail.com>
2024-05-13 16:45:01 +0200

    added logo file

--- um-ios/ContentView.swift
+++ um-ios/ContentView.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            headerView
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
@@ -10,6 +12,19 @@ struct ContentView: View {
         }
         .padding()
     }
+}
+
+private var headerView: some View {
+    HStack {
+        Image("logo-umain")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 56)
+        Spacer()
+    }
+    .padding(.horizontal)
+    .padding(.bottom, 16)
+    .padding(.top, 60)
 }
 
 //#Preview {
