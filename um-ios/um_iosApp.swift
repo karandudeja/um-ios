69a6256eb7ede73d3eb1b81abf79d9736bf171de
karandudeja <learning2make@gmail.com>
2024-05-13 11:40:06 +0200

    Initial Commit

---
+++ um-ios/um_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  um_iosApp.swift
+//  um-ios
+//
+//  Created by DSV on 2024-05-13.
+//
+
+import SwiftUI
+
+@main
+struct um_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
